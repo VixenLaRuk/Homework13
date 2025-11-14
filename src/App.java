ff09ff30c74d57564567a244cd10d0f0bacd3d0c
VixenLaRuk <vixenlaruk@gmail.com>
2025-11-13 22:04:05 -0800

    ostbahnhof

--- src/App.java
+++ src/App.java
@@ -1,7 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * This program creates Tile objects for a given name (VIXEN),
+ * displays the tiles, and calculates the total point value based on the Scrabble game.
+ * @author Vixen La Ruk
+ * @since 11/11/2025
+ */
 public class App {
+    
+    /**
+     * The main entry point of the application.
+     * Creates Tile objects for the letters in "VIXEN" with point values,
+     * prints the set of letters, and the total score.
+     * @param args command-line arguments (not used)
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
+        // Create Tile objects for the letters of "VIXEN" with Scrabble values
         System.out.println("Hello, Scrabble!");
-        Tile tileV = new Tile('V',4);
-        System.out.println("Tile: " + tileV);
+        Tile V = new Tile('V',4);
+        Tile I = new Tile('I',1);
+        Tile X = new Tile('X',8);
+        Tile E = new Tile('E',1);
+        Tile N = new Tile('N',1);
+
+        //Store tiles in a list for easy iteration
+        List<Tile> tiles = new ArrayList<>();
+        tiles.add(V);
+        tiles.add(I);
+        tiles.add(X);
+        tiles.add(E);
+        tiles.add(N);
+
+        //Display the tiles
+        System.out.print("Tiles: ");
+        for (Tile tile : tiles) {
+            System.out.print(tile.getLetter());
+        }
+        System.out.println();
+
+        // Calculate and display the total value
+        int totalValue = 0;
+        for (Tile tile : tiles) {
+            totalValue += tile.getValue();
+        }
+        System.out.println("Total value: " + totalValue);
     }
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,33 +1,74 @@
+import java.util.Objects;
+
+/**
+ * This is a class for Scrabble tiles. Each tile has a letter and a point value.
+ * @author Vixen La Ruk
+ * @since 11/11/2025
+ */
 public class Tile {
     private char letter;
     private int value;
 
+    /**
+     * Creates a new tile with a letter and value.
+     * @param letter the letter on the tile
+     * @param value the points for the tile
+     */
     public Tile(char letter, int value) {
         this.letter = letter;
         this.value = value;
     }
 
+    /**
+     * Changes the letter of the tile.
+     * @param letter the new letter
+     */
     public void setLetter(char letter) {
         this.letter = letter;
 
     }
 
+    /**
+     * Changes the point value of the tile.
+     * @param value the new value
+     */
     public void setValue(int value) {
         this.value = value;
     }
 
+    /**
+     * Gets the letter of the tile.
+     * @return the letter
+     */
     public char getLetter() {
-        return this.letter;
+        return letter;
     }
 
+    /**
+     * Gets the point value of the tile.
+     * @return the value
+     */
     public int getValue() {
-        return this.value;
+        return value;
     }
 
-    public boolean equals(Tile other) {
-        return this.letter == other.letter;
+    /**
+     * Checks if this tile the same as another object.
+     * Tiles are the same if they have the same letter and value.
+     * @param obj the object to check
+     * @return true if they are the same, false if not
+     */
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Tile tile = (Tile) obj;
+        return letter == tile.letter && value == tile.value;
     }
 
+    /**
+     * Shows the tile as a string, like "V (value = 4)".
+     * @return the string for the tile
+     */
     public String toString() {
         return String.format("%c  (value = %d)", letter, value);
     }
